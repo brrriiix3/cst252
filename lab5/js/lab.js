63882a3bf364faab135a7b1fb3d056c48dd89ec8
brrriiix3 <briannaxsanchez@gmail.com>
2021-02-10 14:40:19 -0800

    lab5

---
+++ lab5/js/lab.js
@@ -0,0 +1,27 @@
+//
+//Author: Brianna Sanchez Ortega
+//Created: Feb. 10, 2021
+//License: Public Domain
+//
+
+
+
+// Enter the JavaScript Console in your browser.
+// Declare variables to hold the make, model, color, and year of your vehicle, example, var year = 2004
+var make = "Nissan";
+var model = "Sentra";
+var color = "Black";
+var year = 2018;
+
+// Declare a boolean variable ownIt for whether you own it or not.
+var ownIt = true;
+
+// Create a new variable age that subtracts the year from the current year
+var age = 2021 - year;
+
+// Use document.writeln() to neatly output each of your variables, example, document.writeln("Make: " + make + "<br>");
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
