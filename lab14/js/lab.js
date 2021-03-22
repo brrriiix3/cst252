c2a826e45d89001dea8f2b6f0bab480131f02b86
brrriiix3 <briannaxsanchez@gmail.com>
2021-03-22 13:53:56 -0700

    lab14

---
+++ lab14/js/lab.js
@@ -0,0 +1,34 @@
+//
+// Lab 14 create a JavaScript constructor
+// Author: Brianna Sanchez Ortega
+// Date: March 22, 2021
+// License: Public
+
+//constructor named vehicle that takes in the make,
+// name, model, year, color, extras your vehicle has
+// with a method info thst returns the information
+function Vehicle(make, name, model, year, color, extras) {
+  this.make = make;
+  this.name = name;
+  this.model = model;
+  this.year = year;
+  this.color = color;
+  this.extras = extras;
+  this.info = function () {
+    return this.color + " " + this.year + " " + this.make + " " + this.model + " with " + this.extras + " and called " + this.name;
+  }
+}
+
+
+var vehicles = [];
+
+vehicles.push(new Vehicle("Volkswagen", " him Green Car", "Jetta", 1993, "Forest Green", "Taped Windows & a Sunroof"));
+vehicles.push(new Vehicle("Nissan"," her Caroline", "Sentra", 2018, "Dustyyy Black", "Midnight Edition"));
+
+
+outputEl = document.getElementById("output");
+for(var i = 0; i<vehicles.length; i++){
+  var newEl = document.createElement("li");
+  newEl.innerText = vehicles[i].info();
+  outputEl.appendChild(newEl);
+}
