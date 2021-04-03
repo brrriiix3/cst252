4d1e4fb53127da9898cfe14d844760752e0ff63b
brrriiix3 <briannaxsanchez@gmail.com>
2021-04-02 18:42:51 -0700

    lab15

--- lab14/js/lab.js
+++ lab14/js/lab.js
@@ -4,20 +4,36 @@
 // Date: March 22, 2021
 // License: Public
 
+//PROTOTYPING METHOD
 //constructor named vehicle that takes in the make,
 // name, model, year, color, extras your vehicle has
 // with a method info thst returns the information
-function Vehicle(make, name, model, year, color, extras) {
-  this.make = make;
-  this.name = name;
-  this.model = model;
-  this.year = year;
-  this.color = color;
-  this.extras = extras;
-  this.info = function () {
-    return this.color + " " + this.year + " " + this.make + " " + this.model + " with " + this.extras + " and called " + this.name;
+// function Vehicle(make, name, model, year, color, extras) {
+//   this.make = make;
+//   this.name = name;
+//   this.model = model;
+//   this.year = year;
+//   this.color = color;
+//   this.extras = extras;
+//   this.info = function () {
+//     return this.color + " " + this.year + " " + this.make + " " + this.model + " with " + this.extras + " and called " + this.name;
+//   }
+// }
+
+//NEW CLASS METHOD
+class Vehicle {
+  constructor(make, name, model, year, color, extras) {
+    this.make = make;
+    this.name = name;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    this.extras = extras;
   }
-}
+  info () {
+      return this.color + " " + this.year + " " + this.make + " " + this.model + " with " + this.extras + " and called " + this.name;
+  }
+ }
 
 
 var vehicles = [];
---
+++ lab15/js/lab.js
@@ -0,0 +1,5 @@
+//
+// Lab 15 OOP hangman thingsss
+// Author: Brianna Sanchez Ortega
+// Date: April 2, 2021
+// License: Public
