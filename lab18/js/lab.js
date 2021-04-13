3f4bdbc24956963b544d8ab848e8873d38852c85
brrriiix3 <briannaxsanchez@gmail.com>
2021-04-12 21:28:16 -0700

    lab18

---
+++ lab18/js/lab.js
@@ -0,0 +1,10 @@
+//
+// Lab 18 libraries are cool when you can get them to work
+// Author: Brianna Sanchez Ortega
+// Date: April 12, 2021
+// License: Public
+
+
+$( function() {
+  $( "#accordion" ).accordion();
+} );
